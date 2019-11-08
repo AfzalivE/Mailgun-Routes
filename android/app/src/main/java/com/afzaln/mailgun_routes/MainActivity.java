b6c8aca600e80913e65133a439f0b668d1fc3210
Afzal Najam <afzalnaj@gmail.com>
2019-11-08 07:40:26 -0500

    Added fastlane for ios and Android

--- android/app/src/main/java/com/afzaln/mailgun_routes/MainActivity.java
+++ android/app/src/main/java/com/afzaln/mailgun_routes/MainActivity.java
@@ -5,6 +5,9 @@ import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends FlutterActivity {
   @Override
@@ -12,5 +15,8 @@ public class MainActivity extends FlutterActivity {
     super.onCreate(savedInstanceState);
     Fabric.with(this, new Crashlytics());
     GeneratedPluginRegistrant.registerWith(this);
+
+    AppCenter.start(getApplication(), "0100a0b9-c717-4b24-a7ca-f5e076fd4516",
+                    Analytics.class, Crashes.class);
   }
 }
